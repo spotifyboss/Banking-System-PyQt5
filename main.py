935b73bca6680e842e591f4a995453ace8297c99
spotifyboss <120375705+spotifyboss@users.noreply.github.com>
2023-04-03 01:24:38 +0300

    DaniBank v2.0 fully functional
    Fully functional v2.0 Bank System.

--- main.py
+++ main.py
@@ -259,6 +259,7 @@ class Dashboard(QMainWindow):
         self.ui.depositButton.setStyleSheet("")
         self.ui.withdrawButton.setStyleSheet("")
         self.ui.aboutButton.setStyleSheet("")
+        self.ui.paymentButton.setStyleSheet("")
 
         # clear the line edits and spin Boxes
         self.ui.sendMoneyAccountNumberLineEdit.clear()
@@ -268,9 +269,14 @@ class Dashboard(QMainWindow):
         self.ui.depositConfirmPasswordLineEdit.clear()
         self.ui.withdrawAmountSpinBox.setValue(1)
         self.ui.withdrawConfirmPasswordLineEdit.clear()
+        self.ui.toOtherBankAccountNumberLineEdit.clear()
+        self.ui.toOtherBankAmountSpinBox.setValue(1)
+        self.ui.toOtherBankConfirmPasswordLineEdit.clear()
 
         # change current index for the stacked widget
         self.ui.stackedWidget.setCurrentIndex(0)
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(0)
+        self.ui.paymentFrameStackedWidget.setCurrentIndex(0)
 
     def send_money_button(self):
         self.ui.sendMoneyButton.setStyleSheet("background-color: rgb(76, 79, 121);")
@@ -279,6 +285,7 @@ class Dashboard(QMainWindow):
         self.ui.depositButton.setStyleSheet("")
         self.ui.withdrawButton.setStyleSheet("")
         self.ui.aboutButton.setStyleSheet("")
+        self.ui.paymentButton.setStyleSheet("")
 
         # clear the line edits and spin Boxes
         self.ui.sendMoneyAccountNumberLineEdit.clear()
@@ -289,11 +296,23 @@ class Dashboard(QMainWindow):
         self.ui.withdrawAmountSpinBox.setValue(1)
         self.ui.withdrawConfirmPasswordLineEdit.clear()
 
+        self.ui.toOtherBankAccountNumberLineEdit.clear()
+        self.ui.toOtherBankAmountSpinBox.setValue(1)
+        self.ui.toOtherBankConfirmPasswordLineEdit.clear()
+
         self.ui.errorSendMoneyLabel.clear()
         self.ui.errorDepositLabel.clear()
         self.ui.errorWithdrawLabel.clear()
+        self.ui.errorPaymentTypeLabel.clear()
+        self.ui.errorWithdrawLabel_2.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+        self.ui.errorToOtherBanksLabel.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
 
         self.ui.stackedWidget.setCurrentIndex(1)
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(0)
+        self.ui.paymentFrameStackedWidget.setCurrentIndex(0)
 
     def deposit_button(self):
         self.ui.depositButton.setStyleSheet("background-color: rgb(76, 79, 121);")
@@ -302,6 +321,7 @@ class Dashboard(QMainWindow):
         self.ui.myAccountButton.setStyleSheet("")
         self.ui.withdrawButton.setStyleSheet("")
         self.ui.aboutButton.setStyleSheet("")
+        self.ui.paymentButton.setStyleSheet("")
 
         # clear the line edits and spin Boxes
         self.ui.sendMoneyAccountNumberLineEdit.clear()
@@ -312,11 +332,23 @@ class Dashboard(QMainWindow):
         self.ui.withdrawAmountSpinBox.setValue(1)
         self.ui.withdrawConfirmPasswordLineEdit.clear()
 
+        self.ui.toOtherBankAccountNumberLineEdit.clear()
+        self.ui.toOtherBankAmountSpinBox.setValue(1)
+        self.ui.toOtherBankConfirmPasswordLineEdit.clear()
+
         self.ui.errorSendMoneyLabel.clear()
         self.ui.errorDepositLabel.clear()
         self.ui.errorWithdrawLabel.clear()
+        self.ui.errorPaymentTypeLabel.clear()
+        self.ui.errorWithdrawLabel_2.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+        self.ui.errorToOtherBanksLabel.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
 
         self.ui.stackedWidget.setCurrentIndex(2)
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(0)
+        self.ui.paymentFrameStackedWidget.setCurrentIndex(0)
 
     def withdraw_button(self):
         self.ui.withdrawButton.setStyleSheet("background-color: rgb(76, 79, 121);")
@@ -325,6 +357,7 @@ class Dashboard(QMainWindow):
         self.ui.depositButton.setStyleSheet("")
         self.ui.myAccountButton.setStyleSheet("")
         self.ui.aboutButton.setStyleSheet("")
+        self.ui.paymentButton.setStyleSheet("")
 
         # clear the line edits and spin Boxes
         self.ui.sendMoneyAccountNumberLineEdit.clear()
@@ -335,11 +368,23 @@ class Dashboard(QMainWindow):
         self.ui.withdrawAmountSpinBox.setValue(1)
         self.ui.withdrawConfirmPasswordLineEdit.clear()
 
+        self.ui.toOtherBankAccountNumberLineEdit.clear()
+        self.ui.toOtherBankAmountSpinBox.setValue(1)
+        self.ui.toOtherBankConfirmPasswordLineEdit.clear()
+
         self.ui.errorSendMoneyLabel.clear()
         self.ui.errorDepositLabel.clear()
         self.ui.errorWithdrawLabel.clear()
+        self.ui.errorPaymentTypeLabel.clear()
+        self.ui.errorWithdrawLabel_2.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+        self.ui.errorToOtherBanksLabel.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
 
         self.ui.stackedWidget.setCurrentIndex(3)
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(0)
+        self.ui.paymentFrameStackedWidget.setCurrentIndex(0)
 
     def about_button(self):
         self.ui.aboutButton.setStyleSheet("background-color: rgb(76, 79, 121);")
@@ -348,6 +393,7 @@ class Dashboard(QMainWindow):
         self.ui.depositButton.setStyleSheet("")
         self.ui.withdrawButton.setStyleSheet("")
         self.ui.myAccountButton.setStyleSheet("")
+        self.ui.paymentButton.setStyleSheet("")
 
         # clear the line edits and spin Boxes
         self.ui.sendMoneyAccountNumberLineEdit.clear()
@@ -358,11 +404,189 @@ class Dashboard(QMainWindow):
         self.ui.withdrawAmountSpinBox.setValue(1)
         self.ui.withdrawConfirmPasswordLineEdit.clear()
 
+        self.ui.toOtherBankAccountNumberLineEdit.clear()
+        self.ui.toOtherBankAmountSpinBox.setValue(1)
+        self.ui.toOtherBankConfirmPasswordLineEdit.clear()
+
         self.ui.errorSendMoneyLabel.clear()
         self.ui.errorDepositLabel.clear()
         self.ui.errorWithdrawLabel.clear()
+        self.ui.errorPaymentTypeLabel.clear()
+        self.ui.errorWithdrawLabel_2.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+        self.ui.errorToOtherBanksLabel.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
 
         self.ui.stackedWidget.setCurrentIndex(4)
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(0)
+        self.ui.paymentFrameStackedWidget.setCurrentIndex(0)
+
+    def to_other_banks(self):
+        # clear error texts to avoid confusion
+        self.ui.errorPaymentTypeLabel.clear()
+        self.ui.errorWithdrawLabel_2.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+        self.ui.errorToOtherBanksLabel.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
+
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(1)
+
+    def pay_water_bill(self):
+        if self.ui.waterBillPaymentCodeLineEdit.text() == "":
+            self.ui.errorWaterBillPaymentLabel.setText("Please Enter Payment Code")
+        elif self.ui.waterBillConfirmPasswordLineEdit.text() == "":
+            self.ui.errorWaterBillPaymentLabel.setText("Please Enter Password")
+        elif self.ui.waterBillConfirmPasswordLineEdit.text() != return_password(self.account_number):
+            self.ui.errorWaterBillPaymentLabel.setText("Incorrect Password")
+        elif int(self.ui.waterBillAmountLineEdit.text().strip(' Birr')) > int(
+                return_current_amount(self.account_number)):
+            self.ui.errorWaterBillPaymentLabel.setText("Withdraw Amount Exceeds Deposit")
+        else:
+            withdraw(self.account_number, self.ui.waterBillAmountLineEdit.text().strip(' Birr'))
+            self.ui.errorWaterBillPaymentLabel.setText(
+                "Paid for code " + self.ui.waterBillPaymentCodeLineEdit.text() + " with \n" +
+                self.ui.waterBillAmountLineEdit.text() + " Successfully")
+
+            # refresh the current amount in myAccount page
+            self.ui.myAccountCurrentAmountLabel.setText(
+                '<html><head/><body><p align="center"><span style=" font-size:12pt;'
+                'font-weight:600;">' + return_current_amount(self.account_number)
+                + ' Birr' + '</span></p></body></html>')
+
+    def pay_school_tuition(self):
+        if self.ui.schoolTuitionAccountNumberLineEdit.text() == "":
+            self.ui.errorSchoolTuitionLabel.setText("Please Enter Payment Code")
+        elif self.ui.schoolTuitionConfirmPassword.text() == "":
+            self.ui.errorSchoolTuitionLabel.setText("Please Enter Password")
+        elif self.ui.schoolTuitionConfirmPassword.text() != return_password(self.account_number):
+            self.ui.errorSchoolTuitionLabel.setText("Incorrect Password")
+        elif os.path.exists("./data/" + str(self.ui.schoolTuitionAccountNumberLineEdit.text())) is False:
+            self.ui.errorSchoolTuitionLabel.setText("School Account Does not exist")
+        elif self.account_number == str(self.ui.schoolTuitionAccountNumberLineEdit.text()):
+            self.ui.errorSchoolTuitionLabel.setText("Cannot Pay to yourself")
+        elif int(self.ui.schoolTuitionAmountLineEdit.text().strip(' Birr')) > int(
+                return_current_amount(self.account_number)):
+            self.ui.errorSchoolTuitionLabel.setText("Withdraw Amount Exceeds Deposit")
+        else:
+            withdraw(self.account_number, self.ui.schoolTuitionAmountLineEdit.text().strip(' Birr'))
+            self.ui.errorSchoolTuitionLabel.setText(
+                "Paid for school with account no. " + self.ui.schoolTuitionAccountNumberLineEdit.text() + " with \n" +
+                self.ui.schoolTuitionAmountLineEdit.text() + " for month " +
+                self.ui.schoolTuitionMonthComboBox.currentText()
+                + " Successfully")
+
+            # refresh the current amount in myAccount page
+            self.ui.myAccountCurrentAmountLabel.setText(
+                '<html><head/><body><p align="center"><span style=" font-size:12pt;'
+                'font-weight:600;">' + return_current_amount(self.account_number)
+                + ' Birr' + '</span></p></body></html>')
+
+    def pay_mobile_airtime(self):
+        if self.ui.mobileAirTimePhoneNumberLineEdit.text() == "":
+            self.ui.errorMobileAirTimeLabel.setText("Please Enter Phone Number")
+        elif self.ui.mobileAirTimeConfirmPassword.text() == "":
+            self.ui.errorMobileAirTimeLabel.setText("Please Enter Password")
+        elif self.ui.mobileAirTimeConfirmPassword.text() != return_password(self.account_number):
+            self.ui.errorMobileAirTimeLabel.setText("Incorrect Password")
+        else:
+            withdraw(self.account_number, self.ui.mobileAirTimeAmountLineEdit.text().strip(' Birr'))
+
+            self.ui.errorMobileAirTimeLabel.setText(
+                "Recharged " + self.ui.mobileAirTimeAmountLineEdit.text() + " to phone number \n" +
+                self.ui.mobileAirTimePhoneNumberLineEdit.text() + " Successfully")
+
+            # refresh the current amount in myAccount page
+            self.ui.myAccountCurrentAmountLabel.setText(
+                '<html><head/><body><p align="center"><span style=" font-size:12pt;'
+                'font-weight:600;">' + return_current_amount(self.account_number)
+                + ' Birr' + '</span></p></body></html>')
+
+    def payment_button(self):
+        self.ui.paymentButton.setStyleSheet("background-color: rgb(76, 79, 121);")
+
+        self.ui.sendMoneyButton.setStyleSheet("")
+        self.ui.depositButton.setStyleSheet("")
+        self.ui.withdrawButton.setStyleSheet("")
+        self.ui.myAccountButton.setStyleSheet("")
+        self.ui.aboutButton.setStyleSheet("")
+
+        # refresh the combo box everytime payment is clicked
+        self.ui.selectPaymentTypeComboBox.setCurrentIndex(0)
+
+        # give function to the proceed button inside Payment
+        self.ui.paymentProceedButton.clicked.connect(lambda: self.ui.paymentFrameStackedWidget.setCurrentIndex(
+            self.ui.selectPaymentTypeComboBox.currentIndex() + 1))
+
+        # clear the line edits and spin Boxes
+        self.ui.sendMoneyAccountNumberLineEdit.clear()
+        self.ui.sendMoneyAmountSpinBox.setValue(1)
+        self.ui.sendMoneyConfirmPasswordLineEdit.clear()
+        self.ui.depositAmountSpinBox.setValue(1)
+        self.ui.depositConfirmPasswordLineEdit.clear()
+        self.ui.withdrawAmountSpinBox.setValue(1)
+        self.ui.withdrawConfirmPasswordLineEdit.clear()
+
+        self.ui.toOtherBankAccountNumberLineEdit.clear()
+        self.ui.toOtherBankAmountSpinBox.setValue(1)
+        self.ui.toOtherBankConfirmPasswordLineEdit.clear()
+
+        self.ui.errorSendMoneyLabel.clear()
+        self.ui.errorDepositLabel.clear()
+        self.ui.errorWithdrawLabel.clear()
+        self.ui.errorPaymentTypeLabel.clear()
+        self.ui.errorWithdrawLabel_2.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+        self.ui.errorToOtherBanksLabel.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
+
+        # function when pay buttons are clicked
+        self.ui.waterBillPayButton.clicked.connect(lambda: self.pay_water_bill())
+        self.ui.schoolTuitionPayButton.clicked.connect(lambda: self.pay_school_tuition())
+        self.ui.mobileAirTimePayButton.clicked.connect(lambda: self.pay_mobile_airtime())
+
+        # clear the contents inside Payment stacked widget (no need to copy this to all the other function)
+        self.ui.waterBillPaymentCodeLineEdit.clear()
+        self.ui.waterBillAmountLineEdit.setValue(1)
+        self.ui.waterBillConfirmPasswordLineEdit.clear()
+        self.ui.errorWaterBillPaymentLabel.clear()
+        self.ui.schoolTuitionAccountNumberLineEdit.clear()
+        self.ui.schoolTuitionAmountLineEdit.setValue(1)
+        self.ui.schoolTuitionConfirmPassword.clear()
+        self.ui.errorSchoolTuitionLabel.clear()
+        self.ui.mobileAirTimePhoneNumberLineEdit.clear()
+        self.ui.mobileAirTimeAmountLineEdit.setValue(1)
+        self.ui.mobileAirTimeConfirmPassword.clear()
+        self.ui.errorMobileAirTimeLabel.clear()
+
+        self.ui.stackedWidget.setCurrentIndex(5)
+        self.ui.sendMoneyStackedWidget.setCurrentIndex(0)
+        self.ui.paymentFrameStackedWidget.setCurrentIndex(0)
+
+    def send_to_other_banks(self):
+        if self.ui.toOtherBankAccountNumberLineEdit.text() == "":
+            self.ui.errorToOtherBanksLabel.setText("Please Enter Receiver Account")
+        elif self.ui.toOtherBankConfirmPasswordLineEdit.text() == "":
+            self.ui.errorToOtherBanksLabel.setText("Please Enter Password")
+        elif self.ui.toOtherBankConfirmPasswordLineEdit.text() != return_password(self.account_number):
+            self.ui.errorToOtherBanksLabel.setText("Incorrect Password")
+        elif int(self.ui.toOtherBankAmountSpinBox.text().strip(' Birr')) > int(
+                return_current_amount(self.account_number)):
+            self.ui.errorToOtherBanksLabel.setText("Withdraw Amount Exceeds Deposit")
+        else:
+            withdraw(self.account_number, self.ui.toOtherBankAmountSpinBox.text().strip(' Birr'))
+            self.ui.errorToOtherBanksLabel.setText(
+                "Sent " + self.ui.toOtherBankAmountSpinBox.text() + " to Account Number " +
+                self.ui.toOtherBankAccountNumberLineEdit.text() + " in \n" +
+                self.ui.toOtherBankSelectBankComboBox.currentText() + " Successfully")
+
+            # refresh the current amount in myAccount page
+            self.ui.myAccountCurrentAmountLabel.setText(
+                '<html><head/><body><p align="center"><span style=" font-size:12pt;'
+                'font-weight:600;">' + return_current_amount(self.account_number)
+                + ' Birr' + '</span></p></body></html>')
 
     def restore_or_maximize_app(self):
         if self.win_status == 0:
@@ -415,7 +639,7 @@ class Dashboard(QMainWindow):
             self.ui.errorSendMoneyLabel.setText("Cannot Send Money To Self")
         elif int(self.ui.sendMoneyAmountSpinBox.text().strip(' Birr')) > int(
                 return_current_amount(self.account_number)):
-            self.ui.errorWithdrawLabel.setText("Withdraw Amount Exceeds Deposit")
+            self.ui.errorSendMoneyLabel.setText("Withdraw Amount Exceeds Deposit")
         else:
             send_money(self.account_number, self.ui.sendMoneyAccountNumberLineEdit.text(),
                        self.ui.sendMoneyAmountSpinBox.text().strip(' Birr'))
@@ -453,6 +677,10 @@ class Dashboard(QMainWindow):
         self.ui.depositButton.clicked.connect(lambda: self.deposit_button())
         self.ui.withdrawButton.clicked.connect(lambda: self.withdraw_button())
         self.ui.aboutButton.clicked.connect(lambda: self.about_button())
+        self.ui.paymentButton.clicked.connect(lambda: self.payment_button())
+
+        # give function to 'other banks' button inside send money
+        self.ui.toOtherBankSendButton.clicked.connect(lambda: self.send_to_other_banks())
 
         # set my account page's Labels
         self.ui.myAccountFirstNameLabel.setText('<html><head/><body><p align="center"><span style=" font-size:12pt; '
@@ -472,15 +700,23 @@ class Dashboard(QMainWindow):
         self.ui.depositConfirmPasswordLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
         self.ui.withdrawConfirmPasswordLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
         self.ui.sendMoneyConfirmPasswordLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.ui.toOtherBankConfirmPasswordLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.ui.waterBillConfirmPasswordLineEdit.setEchoMode(QLineEdit.EchoMode.Password)
+        self.ui.mobileAirTimeConfirmPassword.setEchoMode(QLineEdit.EchoMode.Password)
+        self.ui.schoolTuitionConfirmPassword.setEchoMode(QLineEdit.EchoMode.Password)
 
         # give function to deposit, withdraw and send money button
         self.ui.depositDepositButton.clicked.connect(lambda: self.deposit())
         self.ui.withdrawWithdrawButton.clicked.connect(lambda: self.withdraw())
         self.ui.sendMoneySendButton.clicked.connect(lambda: self.send())
 
+        # give function to the 'send money to other banks' button
+        self.ui.toOtherBankButton.clicked.connect(lambda: self.to_other_banks())
+        self.ui.toOtherBankBackButton.clicked.connect(lambda: self.ui.sendMoneyStackedWidget.setCurrentIndex(0))
 
-# TODO: connect the account_number for both of the windows
-# TODO: make function for deposit and withdraw
+
+# TODO: Give function to Payments
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
